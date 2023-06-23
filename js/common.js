9dd01ce179395c3c5e25fbc16a371863da29b659
EunJinPark <obliviscor29@naver.com>
2023-06-23 17:14:07 +0900

    전체메뉴보기 js빼고 완성

---
+++ js/common.js
@@ -0,0 +1,5 @@
+// common.js
+
+$('.allMenuBtn').click(function(){
+
+});
--- js/main.js
+++ js/main.js
@@ -1,8 +1,2 @@
 // main.js
-// 배너 슬라이드
-$('.slideList').slick({
-    autoplay: true,
-    slidesToShow : 3,
-    centerMode : true
-    
-})
+
