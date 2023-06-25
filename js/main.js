61253c5a4cd9cbb91b2ea19b03038d731db0704d
EJP <obliviscor29@naver.com>
2023-06-25 21:00:08 +0900

    쇼룸 슬라이드버튼 여자는되는데 남자키즈는 안됨

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 // main.js
 
+// 쇼룸 슬라이드 버튼
+$('.rightBtn').click(function() {
+    var active = $(this).siblings('.showList.active');
+    var next = active.next('.showList');
+  
+    console.log(active);
+    console.log(next);
+    if (next.length === 0) {
+      next = $('.showList').first();
+    }
+  
+    active.removeClass('active');
+    next.addClass('active');
+
+    return false;
+});
+  
