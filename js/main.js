e12024a437d7f82357c38755800b9b0bf2451299
EunJinPark <obliviscor29@naver.com>
2023-06-26 14:19:39 +0900

    쇼룸 오왼버튼 완료

--- js/main.js
+++ js/main.js
@@ -2,18 +2,30 @@
 
 // 쇼룸 슬라이드 버튼
 $('.rightBtn').click(function() {
-    var active = $(this).siblings('.showList.active');
-    var next = active.next('.showList');
-  
-    console.log(active);
-    console.log(next);
-    if (next.length === 0) {
-      next = $('.showList').first();
-    }
-  
-    active.removeClass('active');
-    next.addClass('active');
+  var active = $(this).siblings('.showList.active');
+  var next = active.next('.showList');
 
-    return false;
+  if (next.length === 0) {
+    next = active.parent().find('.showList').first();
+  }
+
+  active.removeClass('active');
+  next.addClass('active');
+
+  return false;
+});
+
+$('.leftBtn').click(function() {
+  var active = $(this).siblings('.showList.active');
+  var prev = active.prev('.showList');
+
+  if (prev.length === 0) {
+    prev = active.parent().find('.showList').last();
+  }
+
+  active.removeClass('active');
+  prev.addClass('active');
+
+  return false;
 });
   
