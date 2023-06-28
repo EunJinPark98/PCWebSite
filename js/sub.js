2c830c40dfb254f15ce0a4d84e8bc0edc3420235
EunJinPark <obliviscor29@naver.com>
2023-06-28 15:30:44 +0900

    우먼즈 배너 슬라이드 넣음

--- js/sub.js
+++ js/sub.js
@@ -22,13 +22,11 @@ $('.selecCate').click(function(){
     }
     return false;
 });
-
 // 카테고리 선택
 $('.selecCate li:not(.on) a').click(function() {
     $('.selecCate li.on').removeClass('on');
     $(this).parent('li').addClass('on');
 });
-
 
 
 // 서브_커뮤니티
@@ -46,9 +44,40 @@ $('.qnaTop5 li').click(function(){
 });
 
 
-
 // 서브_우먼즈
-// 배너 슬라이드
-
+// main1 이미지 슬라이드
+$('.slideList').slick({
+    dots : true,
+    appendDots : '.ms_pager',
+    customPaging : function(slider, i){
+      return i+1 + '/' + slider.slideCount;
+    },
+    prevArrow : '.m1_prev',
+    nextArrow : '.m1_next',
+    autoplay : true,
+    atoplaySpeed : 500,
+    speed : 1000,
+    slidesToShow : 3,
+    centerMode : true
+  })
+  // 메인1 슬라이드 텍스트
+  $('.slideText .on').fadeIn(1000);
+  
+  $('.slideList').on('afterChange', function(event, slick, currentSlide) {
+    $('.slideText .on').removeClass('on').hide();
+    $('.slideText div').eq(currentSlide).addClass('on').fadeIn(1000);
+  });
+  // 메인1 슬라이드 재생버튼
+  $('.m1_play').click(function(){
+    $('.slideList').slick('slickPlay');
+    $(this).hide();
+    $('.m1_pause').show();
+  });
+  // 메인1 슬라이드 멈춤버튼
+  $('.m1_pause').click(function(){
+    $('.slideList').slick('slickPause');
+    $(this).hide();
+    $('.m1_play').show();
+  });
   
 
