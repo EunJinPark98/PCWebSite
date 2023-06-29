8f5c430fc332ff576281f9d97e450845e9d1f829
EJP <obliviscor29@naver.com>
2023-06-29 16:46:49 +0900

    수정끝

--- js/sub.js
+++ js/sub.js
@@ -1,34 +1,4 @@
 // sub.js
-
-// 서브_베스트아이템
-// 카테고리 열고 닫기
-$('.selecCate').click(function(){
-    var $ul = $(this).find('ul');
-    
-    if ($ul.height() === 190) {
-        $ul.animate({
-            height: '20px'
-        });
-        $(this).find('li:not(.on) a').css({
-            display: 'none'
-        });
-    } else {
-        $ul.animate({
-            height: '190px'
-        });
-        $(this).find('li a').css({
-            display: 'block'
-        });
-    }
-    return false;
-});
-// 카테고리 선택
-$('.selecCate li:not(.on) a').click(function() {
-    $('.selecCate li.on').removeClass('on');
-    $(this).parent('li').addClass('on');
-});
-
-
 // 서브_커뮤니티
 // 자주 찾는 질문 클릭
 $('.qnaTop5 li').click(function(){
@@ -42,7 +12,6 @@ $('.qnaTop5 li').click(function(){
     };
     return false;
 });
-
 
 // 서브_우먼즈
 // main1 이미지 슬라이드
